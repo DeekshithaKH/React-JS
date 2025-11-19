8e39de04e14d9bde2855a59da9ee2e10387dd01c
preethampreethas <preethamkhkalgundy@gmail.com>
2025-11-19 22:24:23 +0530

    first commit

---
+++ class1/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Page1 from './Pages/Page1';
+import Page2 from './Pages/Page2';
+import Page3 from './Pages/Page3';
+import Page4 from './Pages/Page4';
+import Page5 from './Pages/Page5';
+
+
+
+function App(props){
+  return (
+    <div>
+      {/* <h1>Welcom to React js </h1>
+      <Page1/>
+      <Page2/>
+      <Page3/>
+      <Page4/> */}
+      <Page5/>
+    </div>
+  )
+}
+
+export default App;
---
+++ class1/src/Pages/Page4.jsx
@@ -0,0 +1,17 @@
+import React, {Component } from "react"
+
+class Page4 extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>class component</h2>
+            </div>
+        )
+    }
+}
+
+export default Page4;
